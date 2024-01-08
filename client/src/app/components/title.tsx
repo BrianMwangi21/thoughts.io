35a8b5729eb1865d9266e8b95d7e9faca8cf726b
kabiru <mwangikabiru21@gmail.com>
2024-01-08 20:41:53 +0300

    Added animation to title

--- client/src/app/components/title.tsx
+++ client/src/app/components/title.tsx
@@ -1,13 +1,28 @@
+"use client"
+
 import Image from "next/image";
+import { useEffect } from "react";
+import { useSpring, animated } from 'react-spring';
 
 export default function Title() {
+  const [fadeBounceAnimation, setFadeBounceAnimation] = useSpring(() => ({
+    from: { opacity: 0, transform: 'translateY(-50px)' },
+    to: { opacity: 1, transform: 'translateY(0px)' },
+    config: { tension: 120, friction: 14, duration: 5000 },
+  }));
+
+  useEffect(() => {
+    setFadeBounceAnimation({ opacity: 1, transform: 'translateY(0px)' });
+  }, [setFadeBounceAnimation]);
+
   return (
-    <Image
-      src="/assets/title.png"
-      alt="Thoughts.io"
-      className="absolute top-0 left-1/2 transform -translate-x-1/2"
-      width={300}
-      height={300}
-    />
+    <animated.div style={fadeBounceAnimation}>
+      <Image
+        src="/assets/title.png"
+        alt="Thoughts.io"
+        width={300}
+        height={300}
+      />
+    </animated.div>
   )
 }
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,8 +2,9 @@ import Title from "./components/title";
 
 export default function Home() {
   return (
-    <main className="relative bg-white flex h-screen flex-col items-center justify-between">
+    <main className="bg-white flex h-screen flex-col items-center justify-between p-6">
       <Title />
+
     </main>
   )
 }
