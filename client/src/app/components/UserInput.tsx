ca2ed1ed9d8ec4749891aaa349cad1c7ce11aab7
kabiru <mwangikabiru21@gmail.com>
2024-01-12 19:37:26 +0300

    Added clouds background

--- client/src/app/components/UserInput.tsx
+++ client/src/app/components/UserInput.tsx
@@ -12,7 +12,7 @@ export default function UserInput() {
   }, [setSlideInAnimation]);
 
   return (
-    <animated.div style={slideInAnimation} className="absolute bottom-0 shadow-2xl h-30 w-9/12 md:w-4/12 flex items-center flex-col gap-4 p-6">
+    <animated.div style={slideInAnimation} className="z-1000 bg-white rounded-t-2xl absolute bottom-0 shadow-2xl h-30 w-9/12 md:w-4/12 flex items-center flex-col gap-4 p-6">
       <p className="text-black text-md">What is on your mind ?</p>
       <input type="text" className="rounded-md text-black text-center p-2 w-full ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-xl" />
     </animated.div>
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,10 +3,13 @@ import UserInput from "./components/UserInput";
 
 export default function Home() {
   return (
-    <main className="bg-white flex h-screen flex-col items-center justify-between p-6">
+    <main className="bg-scroll-container flex h-screen flex-col items-center justify-between p-6">
       <Title />
-
       <UserInput />
+
+      <div className="hidden">
+        <a href="https://www.freepik.com/free-vector/beautiful-clear-blue-sky-background_2176010.htm?query=animated clouds">Image by brgfx</a> on Freepik
+      </div>
     </main>
   )
 }
