e109eeb1d51aa90052c8f0422c987e08edb97b42
kabiru <mwangikabiru21@gmail.com>
2024-01-22 13:28:01 +0300

    Get more ballons in there

--- client/src/app/components/Canvas.tsx
+++ client/src/app/components/Canvas.tsx
@@ -4,16 +4,18 @@ import Image from "next/image";
 import { useEffect, useState } from 'react';
 import { emitter } from '../utils/emitter';
 import { animated, useSpring } from 'react-spring';
-import { Grape_Nuts } from "next/font/google";
+import { Pacifico } from "next/font/google";
 
-const grapeNuts = Grape_Nuts({
+const pacifico = Pacifico({
   weight: '400',
   subsets: ["latin"]
 });
 
 const Balloon = ({ input }: { input: string }) => {
-  const startX = Math.random() * window.innerWidth - 400;
-  const endX = Math.random() * window.innerWidth - 400;
+  const images = ["/assets/ballon1.png", "/assets/ballon2.png", "/assets/ballon3.png"];
+  const offset = window.innerWidth - 400;
+  const startX = Math.random() * offset;
+  const endX = offset - startX;
 
   const [flyAwayAnimation, setFlyAwayAnimation] = useSpring(() => ({
     from: { transform: `translate3d(${startX}px, 100vh, 0)` },
@@ -21,22 +23,23 @@ const Balloon = ({ input }: { input: string }) => {
 
   useEffect(() => {
     setFlyAwayAnimation({
-      transform: `translate3d(${endX}, -100vh, 0)`,
-      config: { tension: 120, friction: 14, duration: 10000 }
+      transform: `translate3d(${endX}px, -100vh, 0)`,
+      config: { tension: 120, friction: 14, duration: 10000 },
     });
   }, [setFlyAwayAnimation, endX]);
+
 
   return (
     <animated.div style={{ ...flyAwayAnimation, position: 'fixed', left: 0, top: 0 }}>
       <div className="relative w-[400px] h-[600px]">
         <Image
-          src="/assets/ballon1.png"
+          src={images[Math.floor(Math.random() * images.length)]}
           alt={input}
           width={400}
           height={600}
         />
-        <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/3 text-white text-5xl font-extra">
-          <span className={grapeNuts.className}>{input.toUpperCase()}</span>
+        <div className="absolute text-center top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/3 text-white text-5xl font-extra">
+          <span className={pacifico.className}>{input.toUpperCase()}</span>
         </div>
       </div>
     </animated.div>
